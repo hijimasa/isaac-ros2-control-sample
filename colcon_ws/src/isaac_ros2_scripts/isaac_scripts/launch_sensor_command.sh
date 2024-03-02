355e1fdfab19efc78f0199cd041daa96dcc746a0
Masaaki Hijikata <hijimasa@gmail.com>
2024-03-02 16:31:18 +0900

    add isaac_ros2_utils

--- colcon_ws/src/isaac_ros2_scripts/isaac_scripts/launch_sensor_command.sh
+++
@@ -1,2 +0,0 @@
-(sleep 1; echo -e 'import launch_sensor\nlaunch_sensor.main("URDF_PATH")\n'; sleep 10) | telnet localhost 8223
-
--- colcon_ws/src/isaac_ros2_scripts/isaac_scripts/spawn_command.sh
+++
@@ -1,2 +0,0 @@
-(sleep 1; echo -e 'import spawn\nspawn.main("URDF_PATH", ROBOT_X, ROBOT_Y, ROBOT_Z, ROBOT_ROLL, ROBOT_PITCH, ROBOT_YAW, ROBOT_FIXED)\n'; sleep 5) | telnet localhost 8223
-

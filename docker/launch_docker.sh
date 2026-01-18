c3f1fe3febb4c5207507e0e14eda047e526336a7
Masaaki Hijikata <hijimasa@gmail.com>
2026-01-18 22:56:41 +0900

    [update] Dockerfile for IsaacSim 5.2.0

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 xhost +local:
 
+HOST_GID=$(stat -c %g "$(pwd)/../colcon_ws")
+
 docker run --name isaac-sim --entrypoint bash -it --gpus all -e "ACCEPT_EULA=Y" --rm --network=host --privileged \
-    -v ~/docker/isaac-sim/cache/kit:/isaac-sim/kit/cache/Kit:rw \
-    -v ~/docker/isaac-sim/cache/ov:/root/.cache/ov:rw \
-    -v ~/docker/isaac-sim/cache/pip:/root/.cache/pip:rw \
-    -v ~/docker/isaac-sim/cache/glcache:/root/.cache/nvidia/GLCache:rw \
-    -v ~/docker/isaac-sim/cache/computecache:/root/.nv/ComputeCache:rw \
-    -v ~/docker/isaac-sim/logs:/root/.nvidia-omniverse/logs:rw \
-    -v ~/docker/isaac-sim/data:/root/.local/share/ov/data:rw \
-    -v ~/docker/isaac-sim/documents:/root/Documents:rw \
+    -v ~/docker/isaac-sim/cache/main:/isaac-sim/.cache:rw \
+    -v ~/docker/isaac-sim/cache/computecache:/isaac-sim/.nv/ComputeCache:rw \
+    -v ~/docker/isaac-sim/logs:/isaac-sim/.nvidia-omniverse/logs:rw \
+    -v ~/docker/isaac-sim/config:/isaac-sim/.nvidia-omniverse/config:rw \
+    -v ~/docker/isaac-sim/data:/isaac-sim/.local/share/ov/data:rw \
+    -v ~/docker/isaac-sim/pkg:/isaac-sim/.local/share/ov/pkg:rw \
+    -u 1234:1234 \
+    --group-add $HOST_GID \
     --env="DISPLAY" \
-    -v $HOME/.Xauthority:/root/.Xauthority:rw \
+    -v $HOME/.Xauthority:/isaac-sim/.Xauthority:rw \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
-    --workdir="/root/colcon_ws" \
-    --volume="$(pwd)/../colcon_ws:/root/colcon_ws" \
-    --volume="$(pwd)/lidar_config/hokuyo:/isaac-sim/exts/omni.isaac.sensor/data/lidar_configs/hokuyo" \
+    --workdir="/isaac-sim/colcon_ws" \
+    --volume="$(pwd)/../colcon_ws:/isaac-sim/colcon_ws" \
     isaac-ros2-image:latest	
